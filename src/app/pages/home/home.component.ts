44ed8046abf72be0021fc404dcace6da27cca9e9
Marcel Reyes Langenhorst <marcelreyeslangenhorst@gmail.com>
2025-09-02 17:40:31 +0200

    refactor(home): remove unused LanguageSwitcherComponent and PrivacyPolicyComponent imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { IntroComponent } from '../../components/intro/intro.component';
-import { LanguageSwitcherComponent } from '../../components/language-switcher/language-switcher.component';
 import { ContactComponent } from '../../components/contact/contact.component';
-import { PrivacyPolicyComponent} from '../../components/privacypolicy/privacypolicy.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AboutmeComponent } from '../../components/aboutme/aboutme.component';
@@ -14,11 +12,9 @@ import { AboutmeComponent } from '../../components/aboutme/aboutme.component';
   standalone: true,
   imports: [
     IntroComponent,
-    LanguageSwitcherComponent,
     SkillsComponent,
     ProjectsComponent,
     ContactComponent,
-    PrivacyPolicyComponent,
     FooterComponent,
     NavbarComponent,
     AboutmeComponent,
