8b9fdfae0ac1a246f23f327c1c50729995f47aac
Marcel Reyes Langenhorst <marcelreyeslangenhorst@gmail.com>
2025-09-08 23:02:12 +0200

    fix(router): restore routes for /rechtliche-hinweise and /impressum (temporary to PrivacyPolicyComponent)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivacyPolicyComponent } from './components/privacypolicy/privacypolicy.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'rechtliche-hinweise', component: PrivacyPolicyComponent },
+  { path: 'impressum', component: PrivacyPolicyComponent },
   { path: '**', redirectTo: '' }
 ];
 
