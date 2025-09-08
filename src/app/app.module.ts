c53e56d35a3b6a49c3ec0d67bc0a876b8e48ddd5
Marcel Reyes Langenhorst <marcelreyeslangenhorst@gmail.com>
2025-09-08 21:01:05 +0200

    chore(app): register RevealDirective in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,19 @@
-// Legacy stub: this project uses standalone bootstrap (main.ts).
-// This file is intentionally empty to avoid NgModule diagnostics.
-export {};
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RevealDirective } from './directives/reveal.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RevealDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
