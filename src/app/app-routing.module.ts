d7b72a0e05750a57a2204b34640fb4db42c20e9f
Marcel Reyes Langenhorst <marcelreyeslangenhorst@gmail.com>
2025-09-08 20:02:18 +0200

    feat(navbar): add router links for impressum and privacy policy; unify logo navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivacyPolicyComponent } from './components/privacypolicy/privacypolicy.component';
+import { LegalComponent } from './components/legal/legal.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'rechtliche-hinweise', component: PrivacyPolicyComponent },
+  { path: 'impressum', component: LegalComponent },
   { path: '**', redirectTo: '' }
 ];
 
